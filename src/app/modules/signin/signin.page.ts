f5965a3add58633e04e871bf7a19a26f58188048
Andres Mejias <aemabit@gmail.com>
2020-10-26 15:00:04 -0400

    signin

--- src/app/modules/signin/signin.page.ts
+++ src/app/modules/signin/signin.page.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-signin',
-  templateUrl: './signin.page.html',
-  styleUrls: ['./signin.page.scss'],
+  selector: "app-signin",
+  templateUrl: "./signin.page.html",
+  styleUrls: ["./signin.page.scss"],
 })
 export class SigninPage implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  private emailPattern: any = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  ngOnInit() {
+  constructor() {
+    this.loginForm = this.createForm();
   }
 
+  ngOnInit(): void {}
+
+  get email() {
+    return this.loginForm.get("email");
+  }
+  get phone() {
+    return this.loginForm.get("phone");
+  }
+
+  createForm() {
+    return new FormGroup({
+      email: new FormControl("", [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.pattern(this.emailPattern),
+      ]),
+      phone: new FormControl("", [
+        Validators.required,
+        Validators.minLength(5),
+      ]),
+    });
+  }
+
+  onSubmitLoginForm() {
+    if (this.loginForm.valid) {
+      this.onResetForm();
+    }
+  }
+
+  onResetForm(): void {
+    this.loginForm.reset();
+  }
 }
