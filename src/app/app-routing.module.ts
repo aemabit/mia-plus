06dcfd82dc7356d50e010ccf77761de224586dfc
Andres Mejias <aemabit@gmail.com>
2020-10-27 16:11:17 -0400

    upload docs ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,46 +1,58 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'navigation',
-    pathMatch: 'full'
+    path: "",
+    redirectTo: "navigation",
+    pathMatch: "full",
   },
   {
-    path: 'navigation',
-    loadChildren: () => import('./modules/home-selector/home-selector.module').then( m => m.HomeSelectorPageModule)
+    path: "navigation",
+    loadChildren: () =>
+      import("./modules/home-selector/home-selector.module").then(
+        (m) => m.HomeSelectorPageModule
+      ),
   },
   {
-    path: 'signin',
-    loadChildren: () => import('./modules/signin/signin.module').then( m => m.SigninPageModule)
+    path: "signin",
+    loadChildren: () =>
+      import("./modules/signin/signin.module").then((m) => m.SigninPageModule),
   },
   {
-    path: 'titular',
-    loadChildren: () => import('./modules/form/form-titular/form-titular.module').then( m => m.FormTitularPageModule)
+    path: "titular",
+    loadChildren: () =>
+      import("./modules/form/form-titular/form-titular.module").then(
+        (m) => m.FormTitularPageModule
+      ),
   },
   {
-    path: 'dependent',
-    loadChildren: () => import('./modules/form/form-dependent/form-dependent.module').then( m => m.FormDependentPageModule)
+    path: "dependent",
+    loadChildren: () =>
+      import("./modules/form/form-dependent/form-dependent.module").then(
+        (m) => m.FormDependentPageModule
+      ),
   },
   {
-    path: 'employment',
-    loadChildren: () => import('./modules/form/form-employment/form-employment.module').then( m => m.FormEmploymentPageModule)
+    path: "employment",
+    loadChildren: () =>
+      import("./modules/form/form-employment/form-employment.module").then(
+        (m) => m.FormEmploymentPageModule
+      ),
   },
   {
-    path: 'payment',
-    loadChildren: () => import('./modules/form/form-payment/form-payment.module').then( m => m.FormPaymentPageModule)
-  },
-  {
-    path: 'upload-documents',
-    loadChildren: () => import('./modules/form/form-upload-documents/form-upload-documents.module').then( m => m.FormUploadDocumentsPageModule)
+    path: "upload-documents",
+    loadChildren: () =>
+      import(
+        "./modules/form/form-upload-documents/form-upload-documents.module"
+      ).then((m) => m.FormUploadDocumentsPageModule),
   },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/modules/form/form-upload-documents/form-upload-documents.page.ts
+++ src/app/modules/form/form-upload-documents/form-upload-documents.page.ts
@@ -1,15 +1,85 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-form-upload-documents',
-  templateUrl: './form-upload-documents.page.html',
-  styleUrls: ['./form-upload-documents.page.scss'],
+  selector: "app-form-upload-documents",
+  templateUrl: "./form-upload-documents.page.html",
+  styleUrls: ["./form-upload-documents.page.scss"],
 })
 export class FormUploadDocumentsPage implements OnInit {
+  codeForm: FormGroup;
+  codeApproval: boolean = false;
+  userAplicationType: string = "";
 
-  constructor() { }
+  fb;
+  validation1;
+  validation2;
+  validation3;
+  validation4;
 
-  ngOnInit() {
+  constructor() {
+    this.codeForm = this.createForm();
   }
 
+  ngOnInit() {
+    this.codeApproval = false;
+  }
+
+  ionViewDidEnter() {
+    this.codeApproval = false;
+    this.fb = null;
+    this.validation1 = null;
+    this.validation2 = null;
+    this.validation3 = null;
+    this.validation4 = null;
+  }
+
+  get code() {
+    return this.codeForm.get("code");
+  }
+
+  createForm() {
+    return new FormGroup({
+      code: new FormControl("", [Validators.required]),
+    });
+  }
+
+  verifyCode() {
+    console.log(this.code.value);
+    this.codeApproval = true;
+    this.userAplicationType = "other";
+  }
+
+  onFileSelected() {}
+
+  onUploadDocs() {
+    if (
+      this.userAplicationType === "citizen" ||
+      this.userAplicationType === "resident"
+    ) {
+      const valid =
+        this.validation1 && this.validation2 && this.validation3 ? true : false;
+      if (!valid) {
+        console.log("invalid");
+        return null;
+      }
+      console.log(valid);
+    } else if (this.userAplicationType === "other") {
+      const valid =
+        this.validation1 &&
+        this.validation2 &&
+        this.validation3 &&
+        this.validation4
+          ? true
+          : false;
+
+      if (!valid) {
+        console.log("invalid");
+        return null;
+      }
+      console.log(valid);
+    } else {
+      return null;
+    }
+  }
 }
