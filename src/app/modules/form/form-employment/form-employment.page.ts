515d959eb4ca8bf3f42cde2c56c7354e96dfab31
Andres Mejias <aemabit@gmail.com>
2020-10-27 12:35:34 -0400

    employment form:

--- src/app/modules/form/form-employment/form-employment.page.ts
+++ src/app/modules/form/form-employment/form-employment.page.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-form-employment',
-  templateUrl: './form-employment.page.html',
-  styleUrls: ['./form-employment.page.scss'],
+  selector: "app-form-employment",
+  templateUrl: "./form-employment.page.html",
+  styleUrls: ["./form-employment.page.scss"],
 })
 export class FormEmploymentPage implements OnInit {
+  employmentForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.employmentForm = this.createForm();
   }
 
+  ngOnInit() {}
+
+  get employer() {
+    return this.employmentForm.get("employer");
+  }
+  get annualIncome() {
+    return this.employmentForm.get("annualIncome");
+  }
+  get householdTax() {
+    return this.employmentForm.get("householdTax");
+  }
+  get spouseEmployer() {
+    return this.employmentForm.get("spouseEmployer");
+  }
+  get spouseAnnualIncome() {
+    return this.employmentForm.get("spouseAnnualIncome");
+  }
+
+
+  createForm() {
+    return new FormGroup({
+      employer: new FormControl("", [Validators.required]),
+      annualIncome: new FormControl("", [Validators.required]),
+      householdTax: new FormControl("", [Validators.required]),
+      spouseEmployer: new FormControl(""),
+      spouseAnnualIncome: new FormControl(""),
+    });
+  }
+
+  onSubmitEmploymentForm() {
+    if (this.employmentForm.valid) {
+      this.onResetForm();
+    }
+  }
+
+  onResetForm(): void {
+    this.employmentForm.reset();
+  }
 }
