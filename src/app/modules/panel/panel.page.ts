6fc1919e4ccf3fb346cf92f8d858636d37dcc59f
Andres Mejias <aemabit@gmail.com>
2020-11-02 22:25:11 -0500

    panel router'

---
+++ src/app/modules/panel/panel.page.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-panel",
+  templateUrl: "./panel.page.html",
+  styleUrls: ["./panel.page.scss"],
+})
+export class PanelPage implements OnInit {
+  objRouter = [
+    {
+      page: "Titular Information",
+      redirect: "/titular",
+      img: "https://aemabitfolder.sfo2.digitaloceanspaces.com/MIAPLUS/5272.jpg",
+    },
+    {
+      page: "Dependent Information",
+      redirect: "/dependent",
+      img: "https://aemabitfolder.sfo2.digitaloceanspaces.com/MIAPLUS/5220.jpg",
+    },
+    {
+      page: "Employment Information",
+      redirect: "/employment",
+      img: "https://aemabitfolder.sfo2.digitaloceanspaces.com/MIAPLUS/6143.jpg",
+    },
+    {
+      page: "Upload Documents",
+      redirect: "/upload-documents",
+      img: "https://aemabitfolder.sfo2.digitaloceanspaces.com/MIAPLUS/8619.jpg",
+    },
+    {
+      page: "Download Information",
+      redirect: "/panel",
+      img:
+        "https://aemabitfolder.sfo2.digitaloceanspaces.com/MIAPLUS/pdfimagedow.jpg",
+    },
+    {
+      page: "Support",
+      img: "https://aemabitfolder.sfo2.digitaloceanspaces.com/MIAPLUS/6340.jpg"
+    },
+  ];
+
+  constructor() {}
+
+  ngOnInit() {}
+}
